c844678f7926c02b9e1bf85279145113cb979c88
Saviour Essien <essiensaviour.a@gmail.com>
2019-07-27 01:12:01 +0100

    Add JS file for fetching users

--- js/app.js
+++ js/app.js
@@ -1,21 +1,12 @@
-function createNode(element) {
-  return document.createElement(element);
-}
-
-function append(parent, el) {
-return parent.appendChild(el);
-}
-
 const employees = document.querySelector('.employees__single');
 const img = document.querySelector('.employees--img');
 const name = document.querySelector('.employees--name');
 const title = document.querySelector('.employees--title');
-const followers = document.querySelector('.success');
+const admin = document.querySelector('.success');
 
 
 
-
-const url = "https://api.github.com/users?since=135 rel='next'";
+const url = 'https://api.github.com/users';
 fetch(url)
 .then((resp) => resp.json())
 .then(function(data) {
@@ -24,11 +15,8 @@ return employees.map(function(employee) {
   img.src = employee.avatar_url;
   name.textContent = `${employee.login}`;
   title.textContent = `${employee.type}`;
-  followers.textContent = `${employee.site_admin}`;
+  admin.textContent = `${employee.site_admin}`;
   
-  append(img, employees);
-  append(name, employees);
-  append(title, employees);
 })
 })
 .catch(function(error) {
